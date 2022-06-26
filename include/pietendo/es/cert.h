125ade97c0f1b52b2a272bc2a8b70d3558ecfecb
jakcron <jakcron.dev@gmail.com>
2022-06-26 20:07:58 +0800

    Update docs & enum style

--- include/pietendo/es/cert.h
+++ include/pietendo/es/cert.h
@@ -20,11 +20,29 @@ enum ESCertPubKeyType : uint32_t
 	ESCertPubKeyType_ECC233 = 2, /**< ECC 233 bit key */
 };
 
-static const size_t ES_CERT_NAME_SIZE = 64;
-using ESCertName = tc::bn::string<ES_CERT_NAME_SIZE>;
+	/**
+	 * @brief The ASCII encoded certificate name. Padded with nulls.
+	 * @details
+	 * 
+	 * Examples: 
+	 * * __Root__ (__Root__ issuer)
+	 * * __CAxxxxxxxx__ (Certifcate Authority server __xxxxxxxx__)
+	 * * __XSxxxxxxxx__ (Ticket/Transaction server __xxxxxxxx__)
+	 * * __CPxxxxxxxx__ (Content Publishing server __xxxxxxxx__)
+	 * * __MSxxxxxxxx__ (Manufacturing server __xxxxxxxx__)
+	 * * __YYxxxxxxxx__ (Device with of type __YY__ and serial number __xxxxxxxx__)
+	 * 
+	 * __xxxxxxxx__ represents the server/device serial number encoded in hex. (e.g. __XS0000000f__ is ticket server 15).
+	 */
+using ESCertName = tc::bn::string<64>;
 using ESServerId = ESCertName;
 using ESDeviceId = ESCertName;
 
+	/**
+	 * @brief Template for defining padding in ES public key structures.
+	 * 
+	 * @tparam _size Padding size in bytes.
+	 */
 template <size_t _size>
 using ESPubKeyPad = std::array<byte_t, _size>; 
 
@@ -37,7 +55,7 @@ using ESPubKeyPad = std::array<byte_t, _size>;
 	 */
 struct ESCertHeader
 {
-	tc::bn::be32<ESCertPubKeyType> pubKeyType; /**< ESCertPubKeyType */
+	tc::bn::be32<ESCertPubKeyType> pubKeyType; /**< Public key type */
 	union {
 		ESServerId serverId;
 		ESDeviceId deviceId;
@@ -53,7 +71,7 @@ static_assert(sizeof(ESCertHeader) == 72, "ESCertHeader size");
 struct ESCertRsa2048PublicKey
 {
 	Rsa2048PublicKey      pubKey; /**< RSA2048 Public Key */
-	ESPubKeyPad<52>       pad;
+	ESPubKeyPad<52>       pad;    /**< Padding */
 };
 static_assert(sizeof(ESCertRsa2048PublicKey) == 312, "ESCertRsa2048PublicKey size");
 
@@ -64,7 +82,7 @@ static_assert(sizeof(ESCertRsa2048PublicKey) == 312, "ESCertRsa2048PublicKey siz
 struct ESCertRsa4096PublicKey
 {
 	Rsa4096PublicKey      pubKey; /**< RSA4096 Public Key */
-	ESPubKeyPad<52>       pad;
+	ESPubKeyPad<52>       pad;    /**< Padding */
 };
 static_assert(sizeof(ESCertRsa4096PublicKey) == 568, "ESCertRsa4096PublicKey size");
 
@@ -75,7 +93,7 @@ static_assert(sizeof(ESCertRsa4096PublicKey) == 568, "ESCertRsa4096PublicKey siz
 struct ESCertEcc233PublicKey
 {
 	Ecc233PublicKey       pubKey; /**< ECC233 Public Key */
-	ESPubKeyPad<60>       pad;
+	ESPubKeyPad<60>       pad;    /**< Padding */
 };
 static_assert(sizeof(ESCertEcc233PublicKey) == 120, "ESCertEcc233PublicKey size");
 
@@ -85,9 +103,9 @@ static_assert(sizeof(ESCertEcc233PublicKey) == 120, "ESCertEcc233PublicKey size"
 	 */
 struct ESRootCert
 {
-	ESSigRsa4096             sig;
-	ESCertHeader             head;
-	ESCertRsa4096PublicKey   body;
+	ESSigRsa4096             sig;  /**< RSA 4096-bit sign of the certificate */
+	ESCertHeader             head; /**< Certificate header */
+	ESCertRsa4096PublicKey   body; /**< Certificate body */
 };
 static_assert(sizeof(ESRootCert) == 1280, "ESRootCert size");
 
@@ -98,9 +116,9 @@ static_assert(sizeof(ESRootCert) == 1280, "ESRootCert size");
 	 */
 struct ESCACert
 {
-	ESSigRsa4096             sig;
-	ESCertHeader             head;
-	ESCertRsa2048PublicKey   body;
+	ESSigRsa4096             sig;  /**< RSA 4096-bit sign of the certificate */
+	ESCertHeader             head; /**< Certificate header */
+	ESCertRsa2048PublicKey   body; /**< Certificate body */
 };
 static_assert(sizeof(ESCACert) == 1024, "ESCACert size");
 
@@ -110,9 +128,9 @@ static_assert(sizeof(ESCACert) == 1024, "ESCACert size");
 	 */
 struct ESCASignedCert
 {
-	ESSigRsa2048             sig;
-	ESCertHeader             head;
-	ESCertRsa2048PublicKey   body;
+	ESSigRsa2048             sig;  /**< RSA 2048-bit sign of the certificate */
+	ESCertHeader             head; /**< Certificate header */
+	ESCertRsa2048PublicKey   body; /**< Certificate body */
 };
 static_assert(sizeof(ESCASignedCert) == 768, "ESCASignedCert size");
 
@@ -123,9 +141,9 @@ static_assert(sizeof(ESCASignedCert) == 768, "ESCASignedCert size");
 	 */
 struct ESDeviceCert
 {
-	ESSigRsa2048             sig;
-	ESCertHeader             head;
-	ESCertEcc233PublicKey    body;
+	ESSigRsa2048             sig;  /**< RSA 2048-bit sign of the certificate */
+	ESCertHeader             head; /**< Certificate header */
+	ESCertEcc233PublicKey    body; /**< Certificate body */
 };
 static_assert(sizeof(ESDeviceCert) == 576, "ESDeviceCert size");
 
@@ -136,9 +154,9 @@ static_assert(sizeof(ESDeviceCert) == 576, "ESDeviceCert size");
 	 */
 struct ESDeviceSignedCert
 {
-	ESSigEcc233              sig;
-	ESCertHeader             head;
-	ESCertEcc233PublicKey    body;
+	ESSigEcc233              sig;  /**< ECDSA 233-bit sign of the certificate */
+	ESCertHeader             head; /**< Certificate header */
+	ESCertEcc233PublicKey    body; /**< Certificate body */
 };
 static_assert(sizeof(ESDeviceSignedCert) == 384, "ESDeviceSignedCert size");
 
--- include/pietendo/es/sign.h
+++ include/pietendo/es/sign.h
@@ -9,25 +9,22 @@
 #include <pietendo/es/types.h>
 
 namespace pie { namespace es {
+
 	/**
-     * @enum ESSigType
-     * @brief ES Signature type definition
+     * @brief ES Signature type
      */
 enum ESSigType : uint32_t
 {
-    ES_SIGTYPE_RSA4096_SHA1   = 0x00010000, /**< __RSA4096-PKCS1-SHA1__ signature */
-    ES_SIGTYPE_RSA2048_SHA1   = 0x00010001, /**< __RSA2048-PKCS1-SHA1__ signature */
-    ES_SIGTYPE_ECC_SHA1       = 0x00010002, /**< __ECDSA233-SHA1__ signature */
-    ES_SIGTYPE_RSA4096_SHA256 = 0x00010003, /**< __RSA4096-PKCS1-SHA2-256__ signature */
-    ES_SIGTYPE_RSA2048_SHA256 = 0x00010004, /**< __RSA2048-PKCS1-SHA2-256__ signature (note that Switch Ticket has this word swapped) */
-    ES_SIGTYPE_ECC_SHA256     = 0x00010005, /**< __ECDSA233-SHA256__ signature */
-    ES_SIGTYPE_HMAC_SHA1      = 0x00010006, /**< __HMAC-SHA1__ signature */
+    ESSigType_RSA4096_SHA1   = 0x00010000, /**< __RSA4096-PKCS1-SHA1__ signature */
+    ESSigType_RSA2048_SHA1   = 0x00010001, /**< __RSA2048-PKCS1-SHA1__ signature */
+    ESSigType_ECC_SHA1       = 0x00010002, /**< __ECDSA233-SHA1__ signature */
+    ESSigType_RSA4096_SHA256 = 0x00010003, /**< __RSA4096-PKCS1-SHA2-256__ signature */
+    ESSigType_RSA2048_SHA256 = 0x00010004, /**< __RSA2048-PKCS1-SHA2-256__ signature (note that Switch Ticket has this word swapped) */
+    ESSigType_ECC_SHA256     = 0x00010005, /**< __ECDSA233-SHA256__ signature */
+    ESSigType_HMAC_SHA1      = 0x00010006, /**< __HMAC-SHA1__ signature */
 };
 
-static const size_t ES_ISSUER_SIZE = 64;
-
 	/**
-	 * @typedef ESIssuer
 	 * @brief The signature issuer ASCII encoded certificate hierarchy. Padded with nulls.
 	 * @details
 	 * 
@@ -41,11 +38,10 @@ static const size_t ES_ISSUER_SIZE = 64;
 	 * 
 	 * __xxxxxxxx__ represents the server/device serial number encoded in hex. (e.g. __XS0000000f__ is ticket server 15).
 	 */
-using ESIssuer = tc::bn::string<ES_ISSUER_SIZE>;  
+using ESIssuer = tc::bn::string<64>;  
 
 	/**
-	 * @typedef ESSigPad
-	 * @brief Template for defining padding in ES structures.
+	 * @brief Template for defining padding in ES signature structures.
 	 * 
 	 * @tparam _size Padding size in bytes.
 	 */
@@ -55,30 +51,42 @@ using ESSigPad = tc::bn::pad<_size>;
 /* pack to 4 byte boundaries */
 #pragma pack(push,4)
 
+	/**
+	 * @brief RSA2048 ES Signature
+	 * 
+	 */
 struct ESSigRsa2048
 {
-	tc::bn::be32<ESSigType>    sigType; /**< Big-endian encoded pie::es::ESSigType */
-	Rsa2048Sig                 sig;
-	ESSigPad<60>               pad;
-	ESIssuer                   issuer;
+	tc::bn::be32<ESSigType>    sigType; /**< Signature type */
+	Rsa2048Sig                 sig;     /**< Signature data */
+	ESSigPad<60>               pad;     /**< Padding */
+	ESIssuer                   issuer;  /**< Signature issuer */
 };
 static_assert(sizeof(ESSigRsa2048) == 384, "ESSigRsa2048 size");
 
+	/**
+	 * @brief RSA4096 ES Signature
+	 * 
+	 */
 struct ESSigRsa4096
 {
-	tc::bn::be32<ESSigType>    sigType; /**< Big-endian encoded pie::es::ESSigType */
-	Rsa4096Sig                 sig;
-	ESSigPad<60>               pad;
-	ESIssuer                   issuer;
+	tc::bn::be32<ESSigType>    sigType; /**< Signature type */
+	Rsa4096Sig                 sig;     /**< Signature data */
+	ESSigPad<60>               pad;     /**< Padding */
+	ESIssuer                   issuer;  /**< Signature issuer */
 };
 static_assert(sizeof(ESSigRsa4096) == 640, "ESSigRsa4096 size");
 
+	/**
+	 * @brief ECC233 ES Signature
+	 * 
+	 */
 struct ESSigEcc233
 {
-	tc::bn::be32<ESSigType>    sigType; /**< Big-endian encoded pie::es::ESSigType */
-	Ecc233Sig                  sig;
-	ESSigPad<64>               pad;
-	ESIssuer                   issuer;
+	tc::bn::be32<ESSigType>    sigType; /**< Signature type */
+	Ecc233Sig                  sig;     /**< Signature data */
+	ESSigPad<64>               pad;     /**< Padding */
+	ESIssuer                   issuer;  /**< Signature issuer */
 };
 static_assert(sizeof(ESSigEcc233) == 192, "ESSigEcc233 size");
 
